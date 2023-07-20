ca3dfb3065bd1a152576310053e0c399feaabcfa
gpalencia <gpalencia@google.com>
2023-07-20 16:13:17 -0400

    app layout and functioning api call

---
+++ pokemon-api/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) {}
+
+  pokemon:any
+
+  title = 'pokemon-api';
+
+  ngOnInit(){ 
+    let headers = new HttpHeaders({
+      'x-rapidapi-host': 'random-facts2.p.rapidapi.com',
+			'x-rapidapi-key': 'your-api-key'
+    })
+    this.http
+        .get<any>(' https://pokeapi.co/api/v2/pokemon?limit=151', {
+          headers: headers
+        })
+        .subscribe(pokemonData => {
+          this.pokemon = pokemonData.results
+        })
+  }
+}
